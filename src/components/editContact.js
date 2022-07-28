c73cc8e4993d1030ead7fdb165ff5456099dab72
Ezra-code <njerukithinji872@gmail.com>
2022-07-28 07:23:34 +0300

    refactore form code

--- src/components/editContact.js
+++ src/components/editContact.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Edit() {
+    const[contact, setContact] = useState("")
+    const { id } = useParams()
+    useEffect(() => {
+        fetch(`http://localhost:4000/contact/${id}`)
+        .then(r => r.json())
+    .then(data => setContact(data))
+    }, [id])
+    
+    function handleChange(e) {
+        console.log(e.target.value)
+    }
     return (
-        <p><strong>Never stay whaere</strong></p>
+        <div>
+            
+            <form className="edit-form">
+                <h1>EDIT CONTACT</h1>
+                <input type="text" value={contact.id} disabled/>
+                <input type="text" value={contact.fname} onChange={handleChange } />
+                <input type="text" value={contact.lname}/>
+                <input type="number" value={contact.number}/>
+                <input type="email" value={contact.email}/>
+                <input type="text" value={contact.home}/>
+                <input type="text" value={contact.work}/>
+                <input type="text" value={contact.linkedIn}/>
+                <input type="text" value={contact.gitHub}/>
+            </form>
+       </div>
     )
 }
 
--- src/components/newcontact.js
+++ src/components/newcontact.js
@@ -1,68 +1,36 @@
 import React, { useState } from "react";
 
 function NewContact() {
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [contact, setcontact] = useState("")
-    const [email, setEmail] = useState("")
-    const [work, setWork] = useState("")
-    const [home, setHome] = useState("")
-    const [linkedin, setlLinkedIn] = useState("")
-    const [github, setGithub] = useState("")
-    const [pic, setPic] = useState(null)
-    function HandleLastNameChange(e) {
-        setLastName(e.target.value)
+    const [formDta, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        contact: "",
+        email: "",
+        work: "",
+        home: "",
+        linkedin: "",
+        github: "",
+    })
+
+    function HandleChange(event) {
+        const name = event.target.name
+        const value = event.target.value
+        setFormData({ ...formDta, [name]: value })
+        console.log(formDta)
     }
-    
-    function FileSelectedHandler(e) {
-        console.log(e.target.files[0])
-        setPic(e.target.files[0])
-    }
-
-       function HandlefirstNameChange(e) {
-        setFirstName(e.target.value)
-
-       }
-    
-          function HandleEmailChange(e) {
-            setEmail(e.target.value)
-
-          }
-    
-        function HandleContactChange(e) {
-            setcontact(e.target.value)
-               }
-    
-        function HandleWorkChange(e) {
-            setWork(e.target.value)
-            }
-    
-        function HandleHomeChange(e) {
-            setHome(e.target.value)
-            }
-    
-        function HandleLinkedInChange(e) {
-            setlLinkedIn(e.target.value)
-                }
-    
-        function HandleGithubChange(e) {
-            setGithub(e.target.value)
-              }
     
     //handle submission of form
-
     function HandleSubmit(e) {
         e.preventDefault()
         const formData = {
-            fname: firstName,
-            lname: lastName,
-            number: contact,
-            email: email,
-            home: home,
-            work: work,
-            linkedIn: linkedin,
-            gitHub: github,
-            image: pic
+            fname: formDta.firstName,
+            lname: formDta.lastName,
+            number: formDta.contact,
+            email: formDta.email,
+            home: formDta.home,
+            work: formDta.work,
+            linkedIn: formDta.linkedin,
+            gitHub: formDta.github,
         }
 
         console.log(formData)
@@ -74,68 +42,54 @@ function NewContact() {
             },
             body: JSON.stringify(formData)
         })
-
-
-
-        setEmail("")
-        setFirstName("")
-        setLastName("")
-        setGithub("")
-        setHome("")
-        setWork("")
-        setcontact("")
-        setlLinkedIn("")
     }
-
-
 
     //Jsx form data input
     return (
         <div>
             <form onSubmit={HandleSubmit} className="mb-3">
-                <input type="file" require=".png, .jpg, .jpeg" onChange={FileSelectedHandler}/>
                 <div className="name">
                     <div className="left">
                         <label htmlFor="name" className="form-label">First Name:</label> <br/>
-                        <input type="text" placeholder="Enter first name" className="form-control" value={firstName} onChange={HandlefirstNameChange} required/>
+                        <input type="text" placeholder="Enter first name" className="form-control" name="firstName" value={formDta.firstName}  onChange={HandleChange} required/>
                     </div>
                     <div className="right">
                         <label htmlFor="name" className="form-label">Last Name:</label> <br/>
-                        <input type="text" placeholder="Enter last name" className="form-control" value={lastName} onChange={HandleLastNameChange } required/>
+                        <input type="text" placeholder="Enter last name" className="form-control" name="lastName" value={formDta.lastName}  onChange={HandleChange } required/>
                     </div>
                 </div>
                 
                 <div className="contact">
                     <div className="right">
                         <label htmlFor="number" className="form-label">Enter Number</label> <br/>
-                        <input type="number" placeholder="Enter contact number" className="form-control" value={contact} onChange={HandleContactChange} required/>
+                        <input type="number" placeholder="Enter contact number" className="form-control" name="contact" value={formDta.contact}  onChange={HandleChange} required/>
                     </div>
                     <div className="left">
                          <label htmlFor="email" className="form-label">Enter email</label> <br/>
-                        <input type="email" placeholder="Enter contact em..." className="form-control" value={email} onChange={HandleEmailChange } required/>
+                        <input type="email" placeholder="Enter contact em..." className="form-control" name="email" value={formDta.email}  onChange={HandleChange } required/>
                     </div>
                 </div>
 
                 <div className="address">
                     <div className="right">
                         <label htmlFor="occupation" className="form-label">Enter Occupation</label> <br/>
-                        <input type="text" placeholder="Enter contact occup..." className="form-control" value={work} onChange={HandleWorkChange } required/>
+                        <input type="text" placeholder="Enter contact occup..." className="form-control"  name="work" value={formDta.work} onChange={HandleChange } required/>
                     </div>         
                     <div className="left">
                         <label htmlFor="residence" className="form-label">Enter Residence</label> <br/>
-                        <input type="text" placeholder="Enter place of res..." className="form-control" value={home} onChange={HandleHomeChange } required/>
+                        <input type="text" placeholder="Enter place of res..." className="form-control" name="home" value={formDta.home}  onChange={HandleChange } required/>
                    </div>
                 </div>
                 
                 <div className="socials">
                     <div className="right">
                         <label htmlFor="linkedin" className="form-label">Enter linkedin</label> <br/>
-                        <input type="text" placeholder="linkedIn link" className="form-control" value={linkedin} onChange={HandleLinkedInChange } required/>
+                        <input type="text" placeholder="linkedIn link" className="form-control" name="linkedin" value={formDta.linkedin}  onChange={HandleChange } required/>
                     </div>
 
                     <div className="left">
                         <label htmlFor="github" className="form-label">Enter github link</label> <br/>
-                        <input type="text" placeholder="Github link" className="form-control" value={github} onChange={HandleGithubChange } required/>
+                        <input type="text" placeholder="Github link" className="form-control" value={formDta.github} name="github" onChange={HandleChange } required/>
                     </div>
                 </div>
                 
